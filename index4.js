15748130c7a0d18ecd0e2559ee7b5e4b0ca2bf52
houyuhan <angel460360@gmail.com>
2016-07-24 00:13:43 +0800

    0

--- index4.js
+++ index4.js
@@ -95,47 +95,7 @@ app.get('/api/checkaccount', function(request, response) {
 	});
 });
 
-app.get('/api/checkaccount', function(request, response) {
-	var item = {
-	 user : request.query.user
-	}
-	var collection = myDB.collection('login');
-	collection.find({user: request.query.user} , {_id:0 , user:1}).toArray(function(err, docs) {
-		if (err) {
-			response.status(406).send(err).end();
-		} else {
-			var jsArray = new Array();
-            var jsArray = docs; 
-            var docs2 = []; 
-            for(var i = 0; i < jsArray.length; i++){
-                var jsObj = Object();
-                var jsObj = jsArray[i];
-            if(jsObj.user != " " && jsObj.user !=""){
-					docs2 += jsObj.user;
-                }
-                }  
- 
-             if(docs2.length == 0)
-             { 
 
-            st = [{
-            	user : "0"
-            }]
-            response.type('application/json');
-			response.status(200).send(st).end();
-			
-             }
-           
-             else{
-             	st2 = [{
-             		user : "1"
-             	}]
-			        response.type('application/json');
-			        response.status(200).send(st2).end();
-                 }
-		}
-	});
-});
 
 
 app.listen(process.env.PORT || 5000);

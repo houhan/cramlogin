fadd66117f63831b49d38e8d080c11994f23c56e
houyuhan <angel460360@gmail.com>
2016-07-23 23:48:28 +0800

    2

--- index4.js
+++ index4.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var mongodb = require('mongodb');
-var moment = require('moment');
+var ObjectID = require('mongodb').ObjectID;
 var app = express();
+var md5 = require('md5');
+var gcm = require('node-gcm');
+var moment = require('moment');
 
 var mongodbURL = 'mongodb://bob840312:bob0932233875@ds023624.mlab.com:23624/cram';
 
@@ -35,29 +38,25 @@ app.get('/api/insert', function(request, response) {
 });
 
 
-app.get('/api/queryDataPoint',function(request,response){
-	var items = {
+app.get('/api/query', function(request, response) {
+	
+		var item = {
 		user : request.query.user,
-		password : request.query.password,
+		password : md5(request.query.password),
 		desc : request.query.desc
 	}
+
 	var collection = myDB.collection('login');
 	collection.find({user : request.query.user}, {password: 1, _id: 1, desc: 1}).toArray(function(err, docs) {
-	if (err) {
+		if (err) {
 			response.status(406).send(err).end();
 		} else {
 			response.type('application/json');
 			response.status(200).send(docs).end();
 		}
 	});
-});
+}); 
 
-	app.use(express.static(__dirname+'/public'));
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
-	next();
-});
 
 app.listen(process.env.PORT || 5000);
 console.log('port ' + (process.env.PORT || 5000));

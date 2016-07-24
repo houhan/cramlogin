dbe4359c1f28fc469af9fe6521fa4bd5f7dc8981
houyuhan <angel460360@gmail.com>
2016-07-24 20:20:30 +0800

    0

--- Account.js
+++ Account.js
@@ -44,7 +44,7 @@ app.get('/api/queryAccountDataPoint', function(request, response) {
 				var jsObj = jsArray[i];
 				response.type('application/json');
 				if(id == jsObj.user){
-					if(password == jsObj.password){
+					if(id == jsObj.user && password == jsObj.password){
 						response.status(200).send("succeedLogIn");
 						response.end();
 						break;

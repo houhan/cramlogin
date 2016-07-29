3684a771b16db5aaa457dcd7a9df2797bc8863c7
houyuhan <angel460360@gmail.com>
2016-07-29 15:43:08 +0800

    少md5

--- index4.js
+++ index4.js
@@ -2,7 +2,7 @@ var express = require('express');
 var mongodb = require('mongodb');
 var ObjectID = require('mongodb').ObjectID;
 var app = express();
-var md5 = require('md5');
+
 var gcm = require('node-gcm');
 
 var mongodbURL = 'mongodb://houhan:ag460360@ds029745.mlab.com:29745/dbforaccount';
@@ -22,7 +22,7 @@ mongodb.MongoClient.connect(mongodbURL, function(err, db) {
 app.get('/api/insert', function(request, response) {
 	var item = {
 		user : request.query.user,
-		password : md5(request.query.password),
+		password : request.query.password,
 	}
 	var collection = myDB.collection('login');
 	collection.insert(item, function(err, result) {
@@ -40,7 +40,7 @@ app.get('/api/insert', function(request, response) {
 app.get('/api/query', function(request, response) {
 	var item = {
 	user : request.query.user,
-	password : md5(request.query.password),
+	password : request.query.password,
 	}
 	
 	var collection = myDB.collection('login');

e84eca974fa9b269ec187e2a117ffab39cc5394a
houyuhan <angel460360@gmail.com>
2016-11-17 12:04:26 +0800

    room

--- index4.js
+++ index4.js
@@ -27,6 +27,7 @@ app.get('/api/insert', function(request, response) {
 		name : request.query.name,
 		password : md5(request.query.password),
 		minor : request.query.minor,
+		room : request.query.room,
 	}
 	var collection = myDB.collection('login');
 	collection.insert(item, function(err, result) {
@@ -44,6 +45,7 @@ app.get('/api/insert2', function(request, response) {
 	var item = {
 		user : request.query.user,
 		name : request.query.name,
+		room : request.query.room,
 		password : md5(request.query.password),
 	}
 	var collection = myDB.collection('login');
@@ -68,41 +70,21 @@ app.get('/api/insertRegId', function(request, response) {
 	response.status(200).send("Succeed Save"); 
 	response.end();
 
-	/*items.find().toArray(function (err, docs) {
-		if (err) {
-			console.log(err);
-			__sendErrorResponse(response, 406, err);
-		} else {
-			var jsArray = new Array();
-			var jsArray = docs;
-			
-			for(var i = 0; i < jsArray.length; i++){
-				var jsObj = Object();
-				var jsObj = jsArray[i];
-				response.type('application/json');
-				if(id == jsObj.tel){
-					
-					response.status(200).send("Succeed Save");
-					response.end();
-				}
-
-			}
-
-		}
-	});*/
 });
+
 
 //回傳密碼比對，若成功登入將UID、名稱紀錄起來
 app.get('/api/query', function(request, response) {
 	var item = {
 	user : request.query.user,
 	name : request.query.name,
+	room : request.query.room,
 	minor : request.query.minor,
 	password : md5(request.query.password),
 	}
 	
 	var collection = myDB.collection('login');
-	collection.find({user : request.query.user}, {password: 1, _id: 1, name:1, minor:1 , user:1}).toArray(function(err, docs) {
+	collection.find({user : request.query.user}, {password: 1, _id: 1, name:1, minor:1 , room:1, user:1}).toArray(function(err, docs) {
 		if (err) {
 			response.status(406).send(err).end();
 		} else {
@@ -156,57 +138,6 @@ app.get('/api/checkaccount', function(request, response) {
 	});
 });
 
-
-app.get('/api/queryAccountDataPoint', function(request, response) {
-	var items = database.collection('login');
-	var str = request.query.value;
-	var id;
-	var password ;
-	var minor ;
-	var name ;
-	var AccountArray = new Array();
-	var AccountArray = str.split(",");
-
-	id = AccountArray[0];
-	password  = AccountArray[1];
-	minor = AccountArray[2];
-	name = AccoutArray[3];
-
-	var limit = parseInt(request.query.limit, 10) || 100;
-
-	items.find().sort({$natural: -1}).limit(limit).toArray(function (err, docs) {
-		if (err) {
-			console.log(err);
-			__sendErrorResponse(response, 406, err);
-		} else {
-			var jsArray = new Array();
-			var jsArray = docs;
-			for(var i = 0; i < jsArray.length; i++){
-				var jsObj = Object();
-				var jsObj = jsArray[i];
-				response.type('application/json');
-				if(id == jsObj.user){
-					if(password  == jsObj.password ){
-						response.status(200).send("succeedLogIn");
-						response.end();
-						break;
-					}
-					else if(password  != jsObj.password ){
-						response.status(200).send("WarnPassword");
-						response.end();
-						break;
-					}
-				}
-				else if(id != jsObj.user && i == jsArray.length -1){
-					response.status(200).send("WarnId");
-					response.end();
-				}
-
-			}
-
-		}
-	});
-});
 
 //公佈欄顯示
 app.get('/api/querybillboard', function(request, response) {

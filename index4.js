7f309002fb788d3ee505024516ea00c83cb6a8b7
houyuhan <angel460360@gmail.com>
2016-11-24 21:34:58 +0800

    FCM

--- index4.js
+++ index4.js
@@ -333,7 +333,6 @@ app.get('/api/sendfcm',function(request,response,next){
 		        {
 				    'to': deviceId ,
 				    'notification': {
-					    'sound': 'default',
 					    'title': '智慧安心班',
 					    'body': '小孩已抵達安親班囉！'
 					},
@@ -351,10 +350,8 @@ app.get('/api/sendfcm',function(request,response,next){
 		);	
 	};
 
-	//'e1rHgv5SzV0:APA91bEkGeVykCK8leZR_5FFZITY840MM0D-rwr5JOnBvsdRd4dHGVr1v9SlxVtryLC7du_XaPC6F40v3HezqNDDkqdxo2F3xSePwiFnNYkCKCo9W6wo01hk2MflLr75qWRIQVoyJYh4'
 	sendMessageToUser(to);
 	
-
 	response.write('Done!');
 	response.end();		    	
 		    	
